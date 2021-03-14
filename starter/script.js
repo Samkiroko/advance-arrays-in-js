48ed7622e0fc904da75462c0455041176e481452
Samkiroko <kirokodev@gmail.com>
2021-03-14 16:48:00 +0300

    Map

--- starter/script.js
+++ starter/script.js
@@ -82,7 +82,7 @@ displayMovements(account1.movements);
 /////////////////////////////////////////////////
 // LECTURES
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -152,3 +152,26 @@ displayMovements(account1.movements);
 // currncuesUnique.forEach(function (value, _key, map) {
 //   console.log(`${value}: ${value}`);
 // });
+
+/** Data transformation with MAP, Filter and Reduce **/
+
+const eurToUsd = 1.1;
+
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+console.log(movements);
+console.log(movementsUSD);
+
+//  using for
+// const movementsUSDfor = [];
+// for (const mov of movements) {
+//   movementsUSDfor.push(mov * eurToUsd);
+// }
+// console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+console.log(movementsDescriptions);
