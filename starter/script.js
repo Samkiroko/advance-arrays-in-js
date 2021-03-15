8075d773399fab2b440e76f1bc6a8769d57f2887
Samkiroko <kirokodev@gmail.com>
2021-03-15 09:06:06 +0300

    using map filter and reducep challenge

--- starter/script.js
+++ starter/script.js
@@ -220,3 +220,11 @@ for (const mov of movements) {
   balance2 += mov;
 }
 console.log(balance2);
+
+// maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+
+console.log(max);
